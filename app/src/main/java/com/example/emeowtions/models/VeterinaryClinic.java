9af20403e904860fcc3d0b5bd3c4b30656be320a
wzthor <wzthor2002@gmail.com>
2024-10-18 23:21:47 +0800

    Vet role navigation

---
+++ app/src/main/java/com/example/emeowtions/models/VeterinaryClinic.java
@@ -0,0 +1,91 @@
+package com.example.emeowtions.models;
+
+import com.google.firebase.Timestamp;
+
+public class VeterinaryClinic {
+    private String name;
+    private String description;
+    private String address;
+    private String email;
+    private String phoneNumber;
+    private String logoUrl;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    private VeterinaryClinic() {}
+
+    public VeterinaryClinic(String name, String description, String address, String email, String phoneNumber, String logoUrl, Timestamp createdAt, Timestamp updatedAt) {
+        this.name = name;
+        this.description = description;
+        this.address = address;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.logoUrl = logoUrl;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getLogoUrl() {
+        return logoUrl;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setLogoUrl(String logoUrl) {
+        this.logoUrl = logoUrl;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
