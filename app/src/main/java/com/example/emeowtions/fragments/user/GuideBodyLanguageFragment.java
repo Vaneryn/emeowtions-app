861e85983574b5de0d63ca200d829a845331a894
wzthor <wzthor2002@gmail.com>
2024-10-28 18:26:44 +0800

    User guides

--- app/src/main/java/com/example/emeowtions/fragments/user/GuideBodyLanguageFragment.java
+++ app/src/main/java/com/example/emeowtions/fragments/user/GuideBodyLanguageFragment.java
@@ -2,65 +2,73 @@ package com.example.emeowtions.fragments.user;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.emeowtions.R;
+import com.example.emeowtions.databinding.FragmentGuideBodyLanguageBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GuideBodyLanguageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class GuideBodyLanguageFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "GuideBodyLanguageFragment";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // Layout variables
+    private FragmentGuideBodyLanguageBinding binding;
 
     public GuideBodyLanguageFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GuideBodyLanguageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GuideBodyLanguageFragment newInstance(String param1, String param2) {
-        GuideBodyLanguageFragment fragment = new GuideBodyLanguageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        super(R.layout.fragment_guide_body_language);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_guide_body_language, container, false);
+        binding = FragmentGuideBodyLanguageBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        loadData();
+    }
+
+    private void loadData() {
+        // Load stock images into body language image views
+        // Facial Expressions
+        loadImage("https://as2.ftcdn.net/v2/jpg/02/65/24/31/1000_F_265243179_LUgED0G5wyhhiHUFmf8rkl5s7gGHVROh.jpg", binding.imgEarsFlat);
+        loadImage("https://png.pngtree.com/background/20230519/original/pngtree-cat-with-striped-ears-is-looking-up-at-something-in-the-picture-image_2652355.jpg", binding.imgEarsUp);
+        loadImage("https://cdn.shopify.com/s/files/1/0311/9598/8101/files/scared-kitten-with-dilated-pupils_480x480.jpg?v=1638283147", binding.imgLargePupils);
+        loadImage("https://d2zp5xs5cp8zlg.cloudfront.net/image-35009-800.jpg", binding.imgSmallPupils);
+        loadImage("https://icatcare.org/app/uploads/2020/05/joppe-spaa-I5LTVH2mkrA-unsplash-1200x600.jpg", binding.imgEyesNarrowed);
+        loadImage("https://lh6.googleusercontent.com/proxy/K6MTOrYbvO3m5kG9BlbNHgwZW6x66lHfNfHRWlZMzM9IuQx3ZtY8Z3OI2e_oQ1AuwXL3d16yiP5XO2pOY2Q6NVwL830hY6wzY-Hy4SMuiqHi0CxTI-0A", binding.imgMouthFangs);
+
+        // Posture
+        loadImage("https://cdn-fastly.petguide.com/media/2022/02/16/8259958/what-does-a-cats-arched-back-mean.jpg?size=1200x628", binding.imgPostureArchedBack);
+        loadImage("https://external-preview.redd.it/gfNWJWCMW6IjybGyxRxKInu11Bz4w-E8KKYmHqCY0o0.jpg?width=1080&crop=smart&auto=webp&s=75071465f73003334501e6f203ddf79d42d95e1d", binding.imgPostureExposedBelly);
+        loadImage("https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=612x612&w=0&k=20&c=vvM97wWz-hMj7DLzfpYRmY2VswTqcFEKkC437hxm3Cg=", binding.imgPostureNeutral);
+        loadImage("https://images.squarespace-cdn.com/content/v1/5d5208bebb45570001e085c7/54276227-4560-48a6-8ed8-c8b41673120c/scared-cat-Ringwood-Vet-Clinic.jpg", binding.imgPostureSmall);
+        loadImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVW_TKSuBcOldoHkOGBv4F_BXCxd6_Z1gmJw&s", binding.imgPostureStretch);
+
+        // Key Body Parts
+        loadImage("https://bowwowinsurance.com.au/wp-content/uploads/2021/05/shutterstock_1048661324-Ginger-Maine-Coon-Cat-Standing-in-Pose-with-down-tail.jpg", binding.imgTailNeutral);
+        loadImage("https://thumbs.dreamstime.com/b/scared-cat-10216650.jpg", binding.imgTailTucked);
+        loadImage("https://thevillagevets.com/wp-content/uploads/2023/05/cats-tail-shaped-like-question-mark.jpg", binding.imgTailUp);
+    }
+
+    private void loadImage(String url, ImageView imageView) {
+        Glide.with(getContext().getApplicationContext()).load(url).into(imageView);
     }
 }
--- app/src/main/java/com/example/emeowtions/fragments/user/GuideEmotionAnalysisFragment.java
+++ app/src/main/java/com/example/emeowtions/fragments/user/GuideEmotionAnalysisFragment.java
@@ -9,58 +9,28 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.emeowtions.R;
+import com.example.emeowtions.databinding.FragmentGuideEmotionAnalysisBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GuideEmotionAnalysisFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class GuideEmotionAnalysisFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "GuideEmotionAnalysisFragment";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // Layout variables
+    private FragmentGuideEmotionAnalysisBinding binding;
 
     public GuideEmotionAnalysisFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GuideEmotionAnalyzerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GuideEmotionAnalysisFragment newInstance(String param1, String param2) {
-        GuideEmotionAnalysisFragment fragment = new GuideEmotionAnalysisFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        super(R.layout.fragment_guide_emotion_analysis);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_guide_emotion_analysis, container, false);
+        binding = FragmentGuideEmotionAnalysisBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 }
